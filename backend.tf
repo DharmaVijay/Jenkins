23869c42723e1cffe527b99c0ee09579133b04ad
DharmaVijay <115808758+DharmaVijay@users.noreply.github.com>
2024-06-05 23:22:58 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-backend-for-team2"
+    bucket         = "terraformbackendjenkins"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
